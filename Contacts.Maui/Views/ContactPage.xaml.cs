a45c9a3e085eac30530ae183ef9fa34d4d049ec1
AuniAzis <nurulauniazis@gmail.com>
2024-06-23 00:54:09 +0800

    section 2 done

--- Contacts.Maui/Views/ContactPage.xaml.cs
+++ Contacts.Maui/Views/ContactPage.xaml.cs
@@ -1,4 +1,5 @@
 using Contacts.Maui.Models;
+using System.Collections.ObjectModel;
 using Contact = Contacts.Maui.Models.Contact;
 
 namespace Contacts.Maui.Views;
@@ -16,30 +17,60 @@ public partial class ContactPage : ContentPage
         //    "Tom Hanks",
         //    "Frank Liu"
         //};
+    }
 
-        List<Contact> contacts = ContactRepository.GetContacts();
-        listContacts.ItemsSource = contacts;
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
 
+        SearchBar.Text = string.Empty;
+        LoadContacts();
     }
 
     private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-        
         if(listContacts.SelectedItem != null)
         {
             //logic
             //DisplayAlert("Test", "Test", "ok");
 
-            await Shell.Current.GoToAsync(nameof(EditContactPage));
-        }
-        
-        
+            //await Shell.Current.GoToAsync(nameof(EditContactPage));
+            await Shell.Current.GoToAsync($"{nameof(EditContactPage)}?Id={((Contact)listContacts.SelectedItem).ContactId}");
+        }        
     }
 
     private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
     {
         listContacts.SelectedItem = null;
     }
+
+    private void btnAdd_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+
+    private void Delete_Clicked(object sender, EventArgs e)
+    {
+        var menuItem = sender as MenuItem;
+        var contact = menuItem.CommandParameter as Contact;
+        ContactRepository.DeleteContact(contact.ContactId);
+        LoadContacts();
+    }
+
+    private void LoadContacts()
+    {
+        var contacts = new ObservableCollection<Contact>(ContactRepository.GetContacts());
+        listContacts.ItemsSource = contacts;
+    }
+
+    private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var contacts = new ObservableCollection<Contact>(ContactRepository.SearchContacts(((SearchBar)sender).Text)
+            );
+        listContacts.ItemsSource = contacts;
+    }
+
+
 
     //private void btnEditContact_Clicked(object sender, EventArgs e)
     //{
